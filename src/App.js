70daa058b9b14d7ce270e4515a514898331a7e64
K-osA <evbayooooo@gmail.com>
2022-02-14 00:30:52 +0900

    ThemeProvider

--- src/App.js
+++ src/App.js
@@ -1,11 +1,16 @@
 import styled, { keyframes } from "styled-components";
 
+const Title = styled.h1`
+    color: ${(props) => props.theme.textColor};
+`;
+
 const Wrapper = styled.div`
     display: flex;
     height: 100vh;
     width: 100vw;
     justify-content: center;
     align-items: center;
+    background-color: ${(props) => props.theme.backgroundColor};
 `;
 const animation = keyframes`
     0%{
@@ -44,10 +49,7 @@ const Box = styled.div`
 function App() {
     return (
         <Wrapper>
-            <Box>
-                <Emoji>🤗</Emoji>
-            </Box>
-            <Emoji>🤗</Emoji>
+            <Title>Hello!</Title>
         </Wrapper>
     );
 }
