20e1694f3b22ee63fa050ef3a3c3e3c140bf6a37
K-osA <evbayooooo@gmail.com>
2022-02-13 20:00:27 +0900

    Adapting and Extending

--- src/App.js
+++ src/App.js
@@ -1,32 +1,24 @@
 import styled from "styled-components";
 
 const Father = styled.div`
-  display: flex;
+    display: flex;
 `;
 
-const BoxOne = styled.div`
-  background-color: teal;
-  width: 100px;
-  height: 100px;
+const Box = styled.div`
+    background-color: ${(props) => props.bgColor};
+    width: 100px;
+    height: 100px;
 `;
-
-const BoxTwo = styled.div`
-  background-color: tomato;
-  width: 100px;
-  height: 100px;
-`;
-const Text = styled.h1`
-  color: white;
+const Circle = styled(Box)`
+    border-radius: 50px;
 `;
 function App() {
-  return (
-    <Father>
-      <BoxOne>
-        <Text>Hello</Text>
-      </BoxOne>
-      <BoxTwo />
-    </Father>
-  );
+    return (
+        <Father>
+            <Box bgColor="teal"></Box>
+            <Circle bgColor="tomato" />
+        </Father>
+    );
 }
 
 export default App;
